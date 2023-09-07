2f7970778ac4fa9c0db0d2eab1c9a31f4c728ed0
ejy55n <ejy55n2@gmail.com>
2023-09-07 10:32:35 +0900

    sass설치

--- for-you/src/App.js
+++ for-you/src/App.js
@@ -1,20 +1,11 @@
+import "./assets/style/style.scss";
+import Main from "./pages/Main";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Main />
+      <span>강원교육체입니다.</span>
     </div>
   );
 }
