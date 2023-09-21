53168cf4bc5e4227b1470181dbbb27577ca95b1c
ejy55n <ejy55n2@gmail.com>
2023-09-21 10:13:43 +0900

    style: 배경 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-      <a href="#" className="logo">
-        <h1>업체이름 입력</h1>
-      </a>
+      <Link to="/" className="logo">업체이름 입력</Link>
       <ul className="gnb">
         <li>
-          <Link to="/About"></Link>
+          <Link to="/About">About</Link>
         </li>
         <li>
           <Link to="/Service">Service</Link>
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,10 @@ const About = () => {
     <div className="layout">
       <Header />
       <section>
-        <article>skdhsi</article>
+        <article className="sub-top">
+          <h2 className="sub-top-tit">ABOUT</h2>
+          <p className="sub-top-dec">아이들과 함께 편안하고 즐거운 추억을 만들어보세요.</p>
+        </article>
       </section>
       <Footer />
     </div>
