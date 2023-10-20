cb39855b935a19795ccd6f3c038f00618d424421
ejy55n <ejy55n2@gmail.com>
2023-10-20 09:53:24 +0900

    feat: github.io연동

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./components/Index";
-import About from "./pages/About";
-import Program from "./pages/Program";
-import Service from "./pages/Service";
-import "./assets/style/style.scss";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './components/Index';
+import About from './pages/About';
+import Program from './pages/Program';
+import Service from './pages/Service';
+import './assets/style/style.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Index />} />
         <Route path="/About" element={<About />} />
         <Route path="/Program" element={<Program />} />
         <Route path="/Service" element={<Service />} />
