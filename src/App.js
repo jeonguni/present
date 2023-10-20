3528e246990840aa61268196724d304fbad93dea
ejy55n <ejy55n2@gmail.com>
2023-10-20 10:23:07 +0900

    feat: publishied

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './components/Index';
 import About from './pages/About';
 import Program from './pages/Program';
@@ -8,14 +8,14 @@ import './assets/style/style.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Index />} />
         <Route path="/About" element={<About />} />
         <Route path="/Program" element={<Program />} />
         <Route path="/Service" element={<Service />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
